a264366af1186e5fb74fb4b45261b562810b1f39
fturibiof <fturibiof@gmail>
2019-06-11 12:42:00 -0300

    fixing conflict

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 let variavel = 'Fala Mundão!';
 console.log(variavel);
-=======
-let variavel = 'Fala Mundão';
-console.log(variavel);
->>>>>>> 9fccd505449e6b42cac011f25a22ca1f640033e3
